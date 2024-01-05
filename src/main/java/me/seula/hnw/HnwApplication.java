dfb77931daefa3d1472edbfdce6d4db2c91ef5b0
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-05 21:02:31 +0900

    Initialized Spring Boot

---
+++ src/main/java/me/seula/hnw/HnwApplication.java
@@ -0,0 +1,13 @@
+package me.seula.hnw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HnwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HnwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/seula/hnw/HnwApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seula.hnw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HnwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
