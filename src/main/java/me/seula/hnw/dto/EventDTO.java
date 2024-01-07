aaa983817a60b68772b0afd680b416f151bade12
Sulicious <rheeseula@dgsw.hs.kr>
2024-01-07 17:13:06 +0900

    Feat :: Event 조회 & 삽입

---
+++ src/main/java/me/seula/hnw/dto/EventDTO.java
@@ -0,0 +1,14 @@
+package me.seula.hnw.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EventDTO {
+
+    private String eventName;
+
+    private String eventDesc;
+
+}
--- src/main/java/me/seula/hnw/service/QuestService.java
+++ src/main/java/me/seula/hnw/service/QuestService.java
@@ -5,10 +5,12 @@ import me.seula.hnw.dto.QuestDTO;
 import me.seula.hnw.repository.QuestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class QuestService {
 
     private final QuestRepository questRepository;
