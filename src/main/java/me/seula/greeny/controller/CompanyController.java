3a6bae6c2023fea90329f6065f167fa9a5306114
Sulicious <rheeseula@dgsw.hs.kr>
2024-01-09 16:42:36 +0900

    Fixed :: Product 관련 구현체 정리

--- src/main/java/me/seula/greeny/controller/CompanyController.java
+++ src/main/java/me/seula/greeny/controller/CompanyController.java
@@ -2,10 +2,8 @@ package me.seula.greeny.controller;
 
 import lombok.RequiredArgsConstructor;
 import me.seula.greeny.domain.CompanyEntity;
-import me.seula.greeny.domain.ProductEntity;
 import me.seula.greeny.dto.CompanyDTO;
 import me.seula.greeny.service.CompanyService;
-import me.seula.greeny.service.ProductService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,7 +14,6 @@ import java.util.List;
 public class CompanyController {
 
     private final CompanyService companyService;
-    private final ProductService productService;
 
     @GetMapping
     public List<CompanyEntity> getCompanyList() {
@@ -26,10 +23,6 @@ public class CompanyController {
     @GetMapping("/{companyName}")
     public CompanyEntity getCompany(@PathVariable("companyName") int companyId) {
         return companyService.getCompany(companyId);
-    }
-    @GetMapping("/{companyName}/products")
-    public List<ProductEntity> getCompanyProduct(@PathVariable("companyName") int companyId) {
-        return productService.getCompanyProduct(companyId);
     }
 
     @PostMapping
