d7c91e81c1902bf1a0c413d48d9a32ee4686e73a
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-01-14 02:53:30 +0900

    Feat :: Quest Complete 불러오기 완료
    quest/
    get 요청으로 form-data 형식으로 type 요청 보내기
    type = 1 - 성공  | type = 2 - 진행중 임다

--- src/main/java/me/seula/greeny/domain/QuestEntity.java
+++ src/main/java/me/seula/greeny/domain/QuestEntity.java
@@ -1,10 +1,14 @@
 package me.seula.greeny.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -45,18 +49,29 @@ public class QuestEntity {
     @Column(nullable = false)
     private int questPoint;
 
+    @OneToMany(mappedBy = "questEntity", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+    @JsonIgnore
+    private List<QuestCompleteEntity> questCompleteEntityList = new ArrayList<>();
+
     @Builder
     public QuestEntity(
             String questName,
             String questDesc,
             int questCurrent,
             int questLimit,
-            int questPoint)
+            int questPoint,
+            List<QuestCompleteEntity> questCompleteEntityList)
     {
         this.questName = questName;
         this.questDesc = questDesc;
         this.questCurrent = questCurrent;
         this.questLimit = questLimit;
         this.questPoint = questPoint;
+        this.questCompleteEntityList = questCompleteEntityList;
+    }
+
+    public void addQuestComplete(QuestCompleteEntity questCompleteEntity) {
+        questCompleteEntityList.add(questCompleteEntity);
+        questCompleteEntity.addQuestEntity(this);
     }
 }
--- src/main/java/me/seula/greeny/repository/QuestRepository.java
+++ src/main/java/me/seula/greeny/repository/QuestRepository.java
@@ -2,6 +2,10 @@ package me.seula.greeny.repository;
 
 import me.seula.greeny.domain.QuestEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface QuestRepository extends JpaRepository<QuestEntity, Integer> {
+    List<QuestEntity> findByQuestCompleteEntityListCompleteUserId(int userId);
+
+    List<QuestEntity> findByQuestCompleteEntityListCompleteUserIdIsNullOrQuestCompleteEntityListCompleteUserIdNot(int userId);
 }
