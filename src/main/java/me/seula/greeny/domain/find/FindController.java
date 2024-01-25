188131fa7c9af9d22bbd8a2e53c1a73996e490de
최미래 <miraexhoi@gmail.com>
2024-01-25 20:49:07 +0900

    Improve :: Add @RequestMapping, @RequiredArgsConstructor
    delete constructor

--- src/main/java/me/seula/greeny/domain/find/FindController.java
+++ src/main/java/me/seula/greeny/domain/find/FindController.java
@@ -1,22 +1,20 @@
 package me.seula.greeny.domain.find;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/find")
+@RequiredArgsConstructor
 public class FindController {
     private final FindService findService;
 
-    @Autowired
-    public FindController(FindService findService) {
-        this.findService = findService;
-    }
-
-    @GetMapping("/find")
+    @GetMapping
     public List<Long> getRecommendations(@RequestParam String username) {
         return findService.findDistinctProductIdsByUsernameNot(username);
     }
