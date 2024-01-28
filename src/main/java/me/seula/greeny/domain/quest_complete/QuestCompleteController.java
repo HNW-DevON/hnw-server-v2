fb5593e03a054d6b502080af44ca5839fed04530
Sulicious <rheeseula@dgsw.hs.kr>
2024-01-28 16:32:47 +0900

    Feat :: QuestComplete Swagger 설명 추가

--- src/main/java/me/seula/greeny/domain/quest_complete/QuestCompleteController.java
+++ src/main/java/me/seula/greeny/domain/quest_complete/QuestCompleteController.java
@@ -1,5 +1,6 @@
 package me.seula.greeny.domain.quest_complete;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -12,6 +13,10 @@ public class QuestCompleteController {
 
     private final QuestCompleteService questCompleteService;
 
+    /*
+        퀘스트 완료
+    */
+    @Operation(summary = "퀘스트 완료", description = "퀘스트를 완료 표시합니다")
     @PostMapping("/complete/{id}")
     public ResponseEntity<?> completeQuest(
             Authentication authentication,
