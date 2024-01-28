d0a681e1a200d7d93e49d3e7b70ef1ddc0d7a86c
Sulicious <rheeseula@dgsw.hs.kr>
2024-01-28 16:31:56 +0900

    Feat :: Quest Swagger 설명 추가

--- src/main/java/me/seula/greeny/domain/quest/QuestController.java
+++ src/main/java/me/seula/greeny/domain/quest/QuestController.java
@@ -1,5 +1,6 @@
 package me.seula.greeny.domain.quest;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,10 @@ public class QuestController {
 
     private final QuestService questService;
 
+    /*
+        임무 전체 조회
+    */
+    @Operation(summary = "퀘스트 전체 조회", description = "퀘스트 전체 리스트를 반환합니다")
     @GetMapping("/")
     public List<QuestEntity> getQuestList(
             int type,
@@ -20,11 +25,19 @@ public class QuestController {
         return questService.getQuestList(type, authentication);
     }
 
+    /*
+        특정 퀘스트 조회
+    */
+    @Operation(summary = "특정 퀘스트 조회", description = "특정 퀘스트 정보를 조회합니다")
     @GetMapping("/{questId}")
     public QuestEntity getQuest(@PathVariable("questId") int questId) {
         return questService.getQuest(questId);
     }
 
+    /*
+        퀘스트 생성
+    */
+    @Operation(summary = "퀘스트 생성", description = "퀘스트를 생성합니다")
     @PostMapping
     public void createQuest(QuestDTO questDTO) {
         questService.createQuest(questDTO);
