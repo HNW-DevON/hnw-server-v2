4f91cdf96c23aa05d81d7859f64a0ef15d6c4dc9
Sulicious <rheeseula@dgsw.hs.kr>
2024-01-28 16:26:03 +0900

    Feat :: Pedia Swagger 설명 추가

--- src/main/java/me/seula/greeny/domain/pedia/PediaController.java
+++ src/main/java/me/seula/greeny/domain/pedia/PediaController.java
@@ -1,5 +1,6 @@
 package me.seula.greeny.domain.pedia;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,10 @@ public class PediaController {
 
     private final PediaService pediaService;
 
+    /*
+        자신의 도감 조회
+    */
+    @Operation(summary = "도감 조회", description = "자신의 도감을 조회합니다")
     @GetMapping
     public List<PediaEntity> getPedia() {
         return pediaService.getPedia();
