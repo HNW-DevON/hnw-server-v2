ef25781435bf18e0def6627c3c25ebba8d55662e
Sulicious <rheeseula@dgsw.hs.kr>
2024-01-28 16:27:37 +0900

    Feat :: Point Swagger 설명 추가

--- src/main/java/me/seula/greeny/domain/point/PointController.java
+++ src/main/java/me/seula/greeny/domain/point/PointController.java
@@ -1,5 +1,6 @@
 package me.seula.greeny.domain.point;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,10 @@ public class PointController {
 
     private final PointService pointService;
 
+    /*
+        포인트 적립
+    */
+    @Operation(summary = "인식 포인트 적립", description = "제품 인식 포인트를 적립합니다 (30 포인트)")
     @GetMapping("/get/{productId}")
     public void get_point(@PathVariable("productId") String productId) {
         pointService.updatePoint(30, productId);
