5833d8e614f4a3687ddbd37fe9953d411e007a91
Sulicious <rheeseula@dgsw.hs.kr>
2024-02-04 20:21:46 +0900

    Feat :: 카테고리로 회사 조회 기능

--- src/main/java/me/seula/greeny/domain/company/CompanyRepository.java
+++ src/main/java/me/seula/greeny/domain/company/CompanyRepository.java
@@ -2,6 +2,10 @@ package me.seula.greeny.domain.company;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CompanyRepository extends JpaRepository<CompanyEntity, Integer> {
     Boolean existsByCompanyName(String companyName);
+
+    List<CompanyEntity> findByCompanyCategoryContaining(String companyCategory);
 }
