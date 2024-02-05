b21dd17cf4672950f5d5ba349c7204b5098e9aef
최미래 <miraexhoi@gmail.com>
2024-02-06 03:14:48 +0900

    remove :: remove parameter import문

--- src/main/java/me/seula/greeny/domain/find/FindController.java
+++ src/main/java/me/seula/greeny/domain/find/FindController.java
@@ -1,7 +1,6 @@
 package me.seula.greeny.domain.find;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
