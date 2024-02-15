f055f461f74d4a6c520e1c336922f0f96e77629a
Sulicious <rheeseula@dgsw.hs.kr>
2024-02-15 12:19:26 +0900

    Fix :: CompanyRepository 수정

--- src/main/java/me/seula/greeny/domain/company/repository/CompanyRepository.java
+++ src/main/java/me/seula/greeny/domain/company/repository/CompanyRepository.java
@@ -2,6 +2,7 @@ package me.seula.greeny.domain.company.repository;
 
 import me.seula.greeny.domain.company.entity.CompanyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,4 +10,7 @@ public interface CompanyRepository extends JpaRepository<CompanyEntity, Integer>
     Boolean existsByCompanyName(String companyName);
 
     List<CompanyEntity> findByCompanyCategoryContaining(String companyCategory);
+
+    @Query(value = "SELECT * FROM CompanyEntity ORDER BY RAND() LIMIT 2", nativeQuery = true)
+    List<CompanyEntity> getCompanyListDaily();
 }
